6b2fa6c8ab97c52583f5bf7571705ab9817ada73
Edem <edemdalivo93@gmail.comecho>
2026-01-13 13:42:22 +0000

    Fix final build et fetch2

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+// Garde ces valeurs par défaut, Vercel s'occupe du reste !
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://placeholder.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'placeholder'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    persistSession: false, // Désactive le stockage local pour éviter l'erreur fetch sur Vercel
+    persistSession: false, 
   }
 })
